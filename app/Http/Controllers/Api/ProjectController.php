55b03459b6cdd455a588f7746627e50ff81e33e7
TheDusty117 <gabrieledipassio@gmail.com>
2023-05-02 17:01:41 +0200

    FEAT using with in Project to get the model elem

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -10,10 +10,13 @@ class ProjectController extends Controller
 {
     public function index(){
 
-        $projects = Project::limit(20)->get();  //prendo tutti i prject
+        // $projects = Project::limit(20)->get();  //prendo tutti i prject
+        $projects = Project::with('client','description','category')->orderBy('created_at', 'asc')->paginate(10); //paginate gestisce la paginazione e esegue la query
 
-        return response()->json([   //rendo i project del JSON
-            'projects' => $projects,
+
+        return response()->json([   //trasformo i project in JSON
+            'success' => true,
+            'results' => $projects,
         ]);
 
     }
